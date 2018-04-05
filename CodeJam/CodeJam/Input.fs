28dc9677273b58cd8c4c4812721511d1db057bf0
Jason Manuel <jama.indo@hotmail.com>
2018-04-05 20:56:23 +0800

    Implement test case parsing abstractions
    This will make it easier to parse test cases as the boilerplate will be in one place, and code for each problem will only have to worry about implementing a parser.

---
+++ CodeJam/CodeJam/Input.fs
@@ -0,0 +1,25 @@
+﻿module Input
+    type NumTestCases = | NumberOfCases of int
+    let numCasesToInt (NumberOfCases t) = t
+
+    type TestCase<'a> = | Case of 'a
+    let extractCase (Case case) = case
+
+    type TestCaseParser<'a> = Parser of (string -> TestCaseParser<'a>) | ParsedTestCase of TestCase<'a>
+    type NumberedTestCase<'a> = {case:'a; number:int}
+
+    // Grab the number of test cases
+    let getNumTestCases = System.Console.ReadLine >> int >> NumberOfCases
+
+    let rec getNextTestCase caseParser =
+        let caseParser' = System.Console.ReadLine() |> caseParser
+        match caseParser' with
+        | Parser parser -> getNextTestCase parser
+        | ParsedTestCase case -> case
+
+    let allTestCases caseParser =
+        seq { for number in { 1 .. (getNumTestCases() |> numCasesToInt) }
+            do yield { case = getNextTestCase caseParser |> extractCase; number = number } }
+
+    // Example test case parser (it just captures an integer)
+    let _exampleParser (line:string) = line |> int |> Case |> ParsedTestCase

16a02f3f3fc36c1ffd4c35f2a01959cea5d9e3ff
Jason Manuel <jama.indo@hotmail.com>
2018-04-06 22:42:07 +0800

    Implement output abstraction
    Individual problem solutions will not have to do output themselves. Instead, the calls to the output functions will be in Program.fs.

---
+++ CodeJam/CodeJam/Output.fs
@@ -0,0 +1,4 @@
+﻿module Output
+    type TestCaseOutput = {caseNumber:int; output:string}
+    let outputString caseOut = "Case #" + (string caseOut.caseNumber) + ": " + caseOut.output
+    let printCaseOutput = outputString >> printfn "%s"

a6fe50d744096e3694eef627084609522eaeab5b
Jason Manuel <jama.indo@hotmail.com>
2018-04-07 13:00:39 +0800

    Write a simple command line argument parser
    So that the main program can deal with a type-safe model of the arguments instead of a sequence of strings.

---
+++ CodeJam/CodeJam/CLIArguments.fs
@@ -0,0 +1,14 @@
+﻿module CLIArguments
+    type Problem = | RankAndFile
+    type Arguments = {problem:Problem}
+    type ParsedArgs = Args of Arguments | NoValidArgs
+
+    // don't do dispatch to solution modules here, that's beyond the scope of argument parsing
+
+    let rec parseArgs args =
+        match args with
+        | "--problem"::"rank-and-file"::_ -> Args { problem = RankAndFile }
+        | unrecognized::rest ->
+            eprintfn "Unrecognized command line argument: %s" unrecognized
+            parseArgs rest
+        | [] -> NoValidArgs

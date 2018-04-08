9c92d9ff17c7335c2595b70bae6585de7acf88e3
Jason Manuel <jama.indo@hotmail.com>
2018-04-08 17:16:13 +0800

    Make the parser more thorough in its error handling
    It's still inconsistent, as sometimes it'll just print to stderr and skip the bad argument. I think it shouldn't skip: all errors should be returned to the caller as soon as they are encountered (thus making the parser stricter).

--- CodeJam/CodeJam/CLIArguments.fs
+++ CodeJam/CodeJam/CLIArguments.fs
@@ -1,13 +1,35 @@
 ﻿module CLIArguments
     type Problem = | RankAndFile
     type Arguments = {problem:Problem}
-    type ParsedArgs = Args of Arguments | NoValidArgs
+    type ParsedArgs = 
+        | Args of Arguments
+        | NoValidArgs
+        | RepeatedArg of string
+        | MissingValue of string
 
     // don't do dispatch to solution modules here, that's beyond the scope of argument parsing
 
+    /// Parse command line arguments from a list of arguments.
+    /// The arguments recognized are as follows:
+    /// * --problem <name> : Run the solution to the problem named <name>. <name> can be any of:
+    ///     * rank-and-file
+    ///     If any other name is given, an error message will be printed to standard error and
+    ///     the --problem argument will be ignored. If multiple valid --problem arguments are
+    ///     given, an error message will be printed to standard error and RepeatedArg "--problem"
+    ///     will be returned.
+    /// If no recognized options are parsed, NoValidArgs will be returned.
     let rec parseArgs args =
         match args with
-        | "--problem"::"rank-and-file"::_ -> Args { problem = RankAndFile }
+        | "--problem"::rest ->
+            match rest with
+            | "rank-and-file"::rest ->
+                match parseArgs rest with
+                | Args { problem = _ } -> RepeatedArg "--problem"
+                | _ -> Args { problem = RankAndFile }
+            | unrecognized::rest ->
+                eprintfn "Unrecognized problem name passed to --problem: %s" unrecognized
+                parseArgs rest
+            | [] -> MissingValue "--problem"
         | unrecognized::rest ->
             eprintfn "Unrecognized command line argument: %s" unrecognized
             parseArgs rest

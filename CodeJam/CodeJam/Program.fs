3be026484eeca1a6f97d6ac6c823aedd954dd716
Jason Manuel <jama.indo@hotmail.com>
2018-07-04 21:18:56 -0700

    Pull handling of arg parsing errors into a function
    Allowed me to reduce repetition of print the error message, usage, and exiting.

--- CodeJam/CodeJam/Program.fs
+++ CodeJam/CodeJam/Program.fs
@@ -12,35 +12,38 @@ let printUsage () = printfn "%s" usageMessage
 let solveAndOutput (Solution solution) =
     for output in solution do printCaseOutput output
 
+let handleArgParsingError err =
+    let message =
+        match err with
+        | NoArgs ->
+            "No arguments were passed.\n"
+        | MissingValue arg ->
+            sprintf "Argument %s was not given a value.\n" arg
+        | RepeatedArg arg ->
+            sprintf "Argument %s must not be given more than once.\n" arg
+        | BadArg arg ->
+            sprintf "Argument %s is not recognized.\n" arg
+        | BadValue (arg, value) ->
+            sprintf "Argument %s was passed with the unrecognized value %s.\n" arg value
+        | TooManyArgs ->
+            "Too many arguments were passed.\n"
+        | Args _ ->
+            // the code is quite wrong if it gets here
+            let errMsg =
+                "CLI arguments were parsed but treated as an error. " +
+                "Perhaps a match expression is wrong somewhere (in main?)?"
+            invalidArg "err" errMsg
+    eprintfn "%s" message
+    printUsage()
+    exit 1
+
 [<EntryPoint>]
 let main argv = 
     let args = Array.toList argv |> parseArgs
     let solution =
         match args with
         | Args { problem = RankAndFile } -> _2016_1a_rank_and_file.solution |> Solution
-        | NoArgs ->
-            printUsage()
-            exit 1 // just exit with a non-zero integer instead of throwing an exception
-        | MissingValue arg ->
-            eprintfn "Argument %s was not given a value.\n" arg
-            printUsage()
-            exit 1
-        | RepeatedArg arg ->
-            eprintfn "Argument %s must not be given more than once.\n" arg
-            printUsage()
-            exit 1
-        | BadArg arg ->
-            eprintfn "Argument %s is not recognized.\n" arg
-            printUsage()
-            exit 1
-        | BadValue (arg, value) ->
-            eprintfn "Argument %s was passed with the unrecognized value %s.\n" arg value
-            printUsage()
-            exit 1
-        | TooManyArgs ->
-            eprintfn "Too many arguments were passed.\n"
-            printUsage()
-            exit 1
+        | err -> handleArgParsingError err
     // run solution and print output
     solveAndOutput solution
     0 // return an integer exit code

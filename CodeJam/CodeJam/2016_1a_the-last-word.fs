9fa209547ac887001c1fac584fcc4f99b407a8ee
Jason Manuel <jama.indo@hotmail.com>
2018-12-10 22:39:32 -0700

    Add "The Last Word" solution
    "The Last Word" is from CodeJam 2016, Round 1A. It was problem A.

---
+++ CodeJam/CodeJam/2016_1a_the-last-word.fs
@@ -0,0 +1,27 @@
+﻿module _2016_1a_the_last_word
+    open Input
+    open Output
+
+    type TestData = {str:string}
+
+    let testCaseParser line =
+        { str = line } |> Case |> ParsedTestCase
+
+    let lastWordBuilder (str:string) =
+        let rec lastWordBuilderInner str (lastWord:string) =
+            match str with
+            | "" -> lastWord
+            | _ -> 
+                if str.[0] >= lastWord.[0] then
+                    lastWordBuilderInner str.[1..] (str.[..0] + lastWord)
+                else
+                    lastWordBuilderInner str.[1..] (lastWord + str.[..0])
+        
+        lastWordBuilderInner str.[1..] str.[..0]
+
+    let solution = 
+        seq {
+            for case in allTestCases testCaseParser do
+                let str = case.case.str
+                let lastWord = lastWordBuilder str
+                yield { caseNumber = case.number; output = lastWord } }

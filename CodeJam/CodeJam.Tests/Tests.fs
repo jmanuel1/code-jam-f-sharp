b86785f02a87c3d95d8aa86bdfbfb4f0ff7ac4f3
Jason Manuel <jama.indo@hotmail.com>
2018-12-11 21:08:09 -0700

    Add tests for 'The Last Word'
    These tests aren't the best since they use details that I think should be internal to the last word module.

--- CodeJam/CodeJam.Tests/Tests.fs
+++ CodeJam/CodeJam.Tests/Tests.fs
@@ -3,3 +3,43 @@
 open System
 open Microsoft.VisualStudio.TestTools.UnitTesting
 
+[<TestClass>]
+type ``'The Last Word' Tests`` () =
+
+    (* These test what should be internal *)
+    [<TestMethod>]
+    member this.``The last word from CAB is CAB``() =
+        Assert.AreEqual("CAB", _2016_1a_the_last_word.lastWordBuilder "CAB")
+
+    [<TestMethod>]
+    member this.``The last word from JAM is MJA``() =
+        Assert.AreEqual("MJA", _2016_1a_the_last_word.lastWordBuilder "JAM")
+
+    [<TestMethod>]
+    member this.``Each line is a single test case containing the initial string``() =
+        let (Input.ParsedTestCase (Input.Case { _2016_1a_the_last_word.str = actual })) = 
+            _2016_1a_the_last_word.testCaseParser "TEST"
+        Assert.AreEqual("TEST", actual)
+
+    [<TestMethod>]
+    member this.``Solves and numbers every sample test case``() =
+        (* This takes advantage of an internal detail: the use of Console.In *)
+        let in' = Console.In
+        Console.SetIn(new IO.StringReader(
+            "5\n" +
+            "CAB\n" +
+            "JAM\n" +
+            "CODE\n" +
+            "ABAAB\n" +
+            "CABCBBABC\n"
+        ))
+        let actual = _2016_1a_the_last_word.solution |> Seq.toList
+        let actualOutput = 
+            List.map (fun { Output.output = actual } -> actual) actual
+        let actualNumbers = 
+            List.map (fun { Output.caseNumber = actual } -> actual) actual
+        Assert.AreEqual(["CAB"; "MJA"; "OCDE"; "BBAAA"; "CCCABBBAB"], 
+            List.ofSeq actualOutput, "last word output is wrong")
+        Assert.AreEqual([1; 2; 3; 4; 5], List.ofSeq actualNumbers, 
+            "test output numbered incorrectly")
+        Console.SetIn(in')

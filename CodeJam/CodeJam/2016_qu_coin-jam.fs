f7339b1c5b1468bfc5acc6a27ba8575041fc3267
Jason Manuel <jama.indo@hotmail.com>
2018-12-15 20:13:52 -0700

    Don't use `params` as a variable
    `params` may be a reserved keyword in the future.

--- CodeJam/CodeJam/2016_qu_coin-jam.fs
+++ CodeJam/CodeJam/2016_qu_coin-jam.fs
@@ -4,7 +4,7 @@
     type TestData = {coinLength:int;numOfCoins:int}
 
     let testCaseParser (line:string) =
-        let params = line.Split(' ')
-        let coinLength = params.[0] |> int
-        let numOfCoins = params.[1] |> int
+        let testParams = line.Split(' ')
+        let coinLength = testParams.[0] |> int
+        let numOfCoins = testParams.[1] |> int
         { coinLength = coinLength; numOfCoins = numOfCoins } |> Case |> ParsedTestCase

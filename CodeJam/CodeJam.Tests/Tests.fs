8f86d58c76b39cdfc8c45dbc9a92f1e279fa8949
Jason Manuel <jama.indo@hotmail.com>
2018-12-16 18:21:07 -0700

    Add test for valid jamcoins

--- CodeJam/CodeJam.Tests/Tests.fs
+++ CodeJam/CodeJam.Tests/Tests.fs
@@ -44,25 +44,33 @@ type ``'Coin Jam' Tests`` () =
             _2016_qu_coin_jam.testCaseParser "6 3"
         Assert.AreEqual((6, 3), (actualLen, actualNum))
 
-    //[<TestMethod>]
-    //member this.``Solves and numbers every sample test case``() =
-    //    (* This takes advantage of an internal detail: the use of Console.In *)
-    //    let in' = Console.In
-    //    Console.SetIn(new IO.StringReader(
-    //        "5\n" +
-    //        "CAB\n" +
-    //        "JAM\n" +
-    //        "CODE\n" +
-    //        "ABAAB\n" +
-    //        "CABCBBABC\n"
-    //    ))
-    //    let actual = _2016_1a_the_last_word.solution |> Seq.toList
-    //    let actualOutput = 
-    //        List.map (fun { Output.output = actual } -> actual) actual
-    //    let actualNumbers = 
-    //        List.map (fun { Output.caseNumber = actual } -> actual) actual
-    //    Assert.AreEqual(["CAB"; "MJA"; "OCDE"; "BBAAA"; "CCCABBBAB"], 
-    //        List.ofSeq actualOutput, "last word output is wrong")
-    //    Assert.AreEqual([1; 2; 3; 4; 5], List.ofSeq actualNumbers, 
-    //        "test output numbered incorrectly")
-    //    Console.SetIn(in')
+    [<TestMethod>]
+    member this.``Generates valid jamcoins with correct test case numbers``() =
+        (* This takes advantage of an internal detail: the use of Console.In *)
+        let in' = Console.In
+        Console.SetIn(new IO.StringReader("1\n6 3"))
+        let { Output.output = output; Output.caseNumber = number }::rest = 
+            _2016_qu_coin_jam.solution |> Seq.toList
+
+        Assert.AreEqual(1, number)
+        Assert.IsTrue(List.isEmpty rest)
+
+        let lines = output.Split('\n')
+        let resultsArray = lines |> Array.map (fun line -> line.Split(' '))
+        resultsArray |> Array.iter (fun result ->
+            let coin = result.[0]
+            let factors = result.[1..]
+            Array.iteri (fun i factor ->
+                let radix = i + 2
+                let value = this.parseWithRadix(coin, radix)
+                Assert.AreEqual(0, value % int factor)
+            ) factors
+        )
+        Console.SetIn(in')
+
+    member private this.parseWithRadix(coin, radix) =
+        let folder = fun (acc, place) digit ->
+            (acc + (int digit)*(pown radix place), place - 1)
+        let (value, _) = 
+            List.fold folder (0, String.length coin - 1) <| List.ofSeq coin
+        value

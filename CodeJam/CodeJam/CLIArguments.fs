b206a0861e5edba0554550b7fe7b3f70fb11fb59
Jason Manuel <jama.indo@hotmail.com>
2018-12-17 17:19:16 -0700

    Add Coin Jam to argument parser
    The solution will correspond to `--problem coin-jam`.

--- CodeJam/CodeJam/CLIArguments.fs
+++ CodeJam/CodeJam/CLIArguments.fs
@@ -1,5 +1,5 @@
 ﻿module CLIArguments
-    type Problem = TheLastWord | RankAndFile
+    type Problem = TheLastWord | RankAndFile | CoinJam
     type Arguments = {problem:Problem}
     type ParsedArgs = 
         | Args of Arguments
@@ -10,19 +10,22 @@
         | BadValue of string * string
         | BadArg of string
 
-    // don't do dispatch to solution modules here, that's beyond the scope of argument parsing
+    (* don't do dispatch to solution modules here, that's beyond the scope of 
+       argument parsing *)
 
     /// Parse command line arguments from a list of arguments.
     /// The arguments recognized are as follows:
-    /// * --problem <name> : Run the solution to the problem named <name>. <name> can be any of:
+    /// * --problem <name> : Run the solution to the problem named <name>. 
+    /// <name> can be any of:
     ///     * rank-and-file
     ///     * the-last-word
-    ///     If any other name is given, BadValue ("--problem", "<name>") will be returned. If
-    ///     multiple valid --problem arguments are given, RepeatedArg "--problem"
-    ///     will be returned.
+    ///     * coin-jam
+    ///     If any other name is given, BadValue ("--problem", "<name>") will 
+    ///     be returned. If multiple valid --problem arguments are given, 
+    ///     RepeatedArg "--problem" will be returned.
     /// If no options are given, NoArgs will be returned. --problem must be the
-    /// only option, else TooManyArgs will be returned. If an unrecognized option is given,
-    /// BadArg "<arg>" will be returned.
+    /// only option, else TooManyArgs will be returned. If an unrecognized 
+    /// option is given, BadArg "<arg>" will be returned.
     let rec parseArgs args =
         match args with
         | "--problem"::rest ->
@@ -43,9 +46,17 @@
                 | MissingValue "--problem" -> RepeatedArg "--problem"
                 | NoArgs -> Args { problem = TheLastWord }
                 | _ -> TooManyArgs
+            | "coin-jam"::rest ->
+                match parseArgs rest with
+                | Args { problem = _ }
+                | RepeatedArg "--problem"
+                | BadValue ("--problem", _)
+                | MissingValue "--problem" -> RepeatedArg "--problem"
+                | NoArgs -> Args { problem = CoinJam }
+                | _ -> TooManyArgs
             | unrecognized::_ ->
-                BadValue ("--problem", unrecognized) //eprintfn "Unrecognized problem name passed to --problem: %s" unrecognized
+                BadValue ("--problem", unrecognized)
             | [] -> MissingValue "--problem"
         | unrecognized::_ ->
-            BadArg unrecognized //eprintfn "Unrecognized command line argument: %s" unrecognized
+            BadArg unrecognized
         | [] -> NoArgs

502d7af270d88da1720563921e0ae1ca548b0d59
Jason Manuel <jama.indo@hotmail.com>
2018-12-21 21:48:33 -0700

    Test that a message to stderr is sent on no/invalid arguments

--- CodeJam/CodeJam.Tests/SystemTests.fs
+++ CodeJam/CodeJam.Tests/SystemTests.fs
@@ -49,3 +49,46 @@ type ``When no arguments or invalid arguments are passed``() =
             codejam.Close()
         )
         Console.SetOut(originalOut)
+
+    [<TestMethod>]
+    member this.``Display an error message in stderr``() =
+        let originalError = Console.Error
+        let possibilities = [
+            [||]
+            [|"--problem"|]
+            [|"--problem"; "coin-jam"; "--problem"|]
+            [|"--gibberish"|]
+        ]
+        possibilities |> List.iter (fun args ->
+            let newError = new IO.StringWriter()
+            Console.SetError(newError)
+            (* The following line causes the test runner to abort without
+               telling me why. I think this happens because there is an
+               `exit 1` in handleArgParsingError. *)
+            //Program.main args |> ignore
+            (* Since there is an exit, the test will have to drive the program 
+               from the command line. *)
+            (* Also, this test requires the CodeJam project to be built first. 
+               And the working directory must be the configuration directory. 
+               (That is, the default.)
+               *)
+            let codejamInfo = 
+                new Diagnostics.ProcessStartInfo(
+                    WindowStyle = Diagnostics.ProcessWindowStyle.Hidden,
+                    FileName = "..\\..\\..\\CodeJam\\bin\\Debug\\CodeJam.exe",
+                    Arguments = String.Join(" ", args),
+                    RedirectStandardError = true,
+                    UseShellExecute = false
+                )
+            let codejam = new Diagnostics.Process(StartInfo = codejamInfo)
+            codejam.Start() |> ignore
+            (* to avoid deadlocks: see Process.StandardOutput docs *)
+            let error = new StringBuilder(codejam.StandardError.ReadToEnd())
+            codejam.WaitForExit()
+            error.Append(codejam.StandardError.ReadToEnd()) |> ignore
+            Assert.IsFalse(String.IsNullOrWhiteSpace(string error), 
+                "error was '" + string error + "'")
+            newError.Close()
+            codejam.Close()
+        )
+        Console.SetError(originalError)
